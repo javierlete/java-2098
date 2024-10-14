312019043d72eec7501d9882210de08bc08d085e
HTML <HTML@server.ipartekaula.com>
2024-10-14 13:59:00 +0200

    Ejemplo de características básicas de Java

---
+++ bases/src/bases/HolaMundo.java
@@ -0,0 +1,55 @@
+/**
+ * Paquete donde vamos a poner clases introductorias
+ */
+package bases;
+
+import java.time.LocalDateTime;
+
+/**
+ * Clase de ejemplo básica
+ */
+public class HolaMundo {
+	/**
+	 * Método de inicio cuando arrancas la aplicación
+	 * 
+	 * @param args argumentos de la línea de consola
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Ejemplo básico de uso del lenguaje Java
+		 */
+		System.out.println("Hola desde java-2098");
+
+		double d1; // Declaración de variable
+
+		d1 = 0.1; // Asignación de variable
+
+		double d2 = 0.2; // Inicialización y declaración de variable
+
+		System.out.println(d1 + d2); // d1 + d2 es una expresión
+
+		String nombre = "Javier";
+
+		System.out.println(nombre);
+
+		nombre = "Pepe";
+
+		System.out.println(nombre);
+
+		LocalDateTime ahora = LocalDateTime.now();
+
+		System.out.println(ahora);
+
+		System.out.println(6 ^ 3); // NO es una potencia
+		System.out.println(Math.pow(6, 3)); // Este SÍ
+		
+		System.out.println(6 > 3);
+		
+		int x = 4;
+		
+		System.out.println(1 <= x && x <= 10);
+		
+		System.out.println(++x);
+		System.out.println(x);
+	}
+}
