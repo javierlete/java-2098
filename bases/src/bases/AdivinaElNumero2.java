238d8973d09c07ea46c259f45adb60214879da15
Javier Lete <javier@eskura21.com>
2024-10-16 09:58:11 +0200

    Refactorización del código en funciones

--- bases/src/bases/AdivinaElNumero2.java
+++ bases/src/bases/AdivinaElNumero2.java
@@ -10,46 +10,67 @@ public class AdivinaElNumero2 {
 		// TODO Número máximo de intentos
 		// TODO Poder poner tu nombre en las puntuaciones
 		var puntuaciones = new ArrayList<Integer>();
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		boolean otraPartida;
-		
+
 		do {
 			int pensado = new Random().nextInt(1, 10);
 			int intentos = 0;
 			int introducido;
-			
+
 			do {
-				System.out.print("Dime un número: ");
-				introducido = sc.nextInt();
-				sc.nextLine();
-				
+				introducido = pedirNumero(sc);
+
 				intentos++;
 
-				if (pensado > introducido) {
-					System.out.println("ES MAYOR");
-				} else if (pensado < introducido) {
-					System.out.println("Es menor");
-				} else {
-					System.out.println("HAS ACERTADO");
-					System.out.println("EFECTIVAMENTE EL NÚMERO ERA EL " + pensado);
-					System.out.println("Has necesitado " + intentos + " intentos");
-					
-					puntuaciones.add(intentos);
-				}
+				mostrarResultado(puntuaciones, pensado, intentos, introducido);
 			} while (pensado != introducido);
-			
-			System.out.print("¿Otra partida? (s/N): ");
-			String otra = sc.nextLine();
-			otraPartida = otra.equals("s");
+
+			otraPartida = pedirOtraPartida(sc);
 		} while (otraPartida);
-		
-		System.out.print("Puntuaciones: ");
-		System.out.println(puntuaciones);
-		
-		System.out.println("Gracias por jugar");
-		
+
+		cierre(puntuaciones);
+
 		sc.close();
+	}
+
+	private static void cierre(ArrayList<Integer> puntuaciones) {
+		System.out.print("Puntuaciones: ");
+
+		System.out.println(puntuaciones);
+
+		System.out.println("Gracias por jugar");
+	}
+
+	private static boolean pedirOtraPartida(Scanner sc) {
+		boolean otraPartida;
+		System.out.print("¿Otra partida? (s/N): ");
+		String otra = sc.nextLine();
+		otraPartida = otra.equals("s");
+		return otraPartida;
+	}
+
+	private static void mostrarResultado(ArrayList<Integer> puntuaciones, int pensado, int intentos, int introducido) {
+		if (pensado > introducido) {
+			System.out.println("ES MAYOR");
+		} else if (pensado < introducido) {
+			System.out.println("Es menor");
+		} else {
+			System.out.println("HAS ACERTADO");
+			System.out.println("EFECTIVAMENTE EL NÚMERO ERA EL " + pensado);
+			System.out.println("Has necesitado " + intentos + " intentos");
+
+			puntuaciones.add(intentos);
+		}
+	}
+
+	private static int pedirNumero(Scanner sc) {
+		int introducido;
+		System.out.print("Dime un número: ");
+		introducido = sc.nextInt();
+		sc.nextLine();
+		return introducido;
 	}
 }
