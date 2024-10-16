acde713f32ba4af929db60d32884363cfba71c57
Javier Lete <javier@eskura21.com>
2024-10-16 09:17:06 +0200

    Colecciones

---
+++ bases/src/bases/Colecciones.java
@@ -0,0 +1,40 @@
+package bases;
+
+import java.util.ArrayList;
+
+public class Colecciones {
+	public static void main(String[] args) {
+		var puntuaciones = new ArrayList<Integer>();
+		
+		System.out.println(puntuaciones);
+		System.out.println(puntuaciones.size());
+		
+		puntuaciones.add(5);
+		puntuaciones.add(3);
+		puntuaciones.add(7);
+		puntuaciones.add(1);
+		
+		System.out.println(puntuaciones);
+		System.out.println(puntuaciones.size());
+
+		puntuaciones.remove(2);
+		System.out.println(puntuaciones);
+		System.out.println(puntuaciones.size());
+
+		puntuaciones.add(0, 2);
+		System.out.println(puntuaciones);
+		System.out.println(puntuaciones.size());
+		
+		puntuaciones.set(3, 10);
+		System.out.println(puntuaciones);
+		System.out.println(puntuaciones.size());
+		
+		for(int i = 0; i < puntuaciones.size(); i++) {
+			System.out.println(puntuaciones.get(i));
+		}
+		
+		for(int dato: puntuaciones) {
+			System.out.println(dato);
+		}
+	}
+}
