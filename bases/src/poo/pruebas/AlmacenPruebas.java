a9e1f241f3aeb472e85c5532d8749dfeb1a663ed
Javier Lete <javier@eskura21.com>
2024-10-21 11:24:39 +0200

    Almacen como ejemplo de composición

---
+++ bases/src/poo/pruebas/AlmacenPruebas.java
@@ -0,0 +1,39 @@
+package poo.pruebas;
+
+import poo.pojos.Almacen;
+import poo.pojos.Producto;
+
+public class AlmacenPruebas {
+	public static void main(String[] args) {
+		Almacen bilbao = new Almacen("Bilbao");
+
+		bilbao.meterProducto(new Producto("Teclado", 34.56));
+		bilbao.meterProducto(new Producto("Alfombrilla de ratón", 4.99));
+
+		System.out.println(bilbao);
+
+		System.out.println(bilbao.getNombre());
+
+		for (Producto p : bilbao.getProductos()) {
+			System.out.println(p.getNombre() + ": " + p.getPrecio());
+		}
+
+		Producto productoBuscado = bilbao.getProductoPorId(1L);
+
+		if (productoBuscado != null) {
+			System.out.println(productoBuscado);
+		} else {
+			System.out.println("No se ha encontrado el producto");
+		}
+		
+		System.out.println(bilbao);
+		
+		bilbao.eliminarProducto(1L);
+		
+		System.out.println(bilbao);
+		
+		bilbao.modificarProducto(new Producto(2L, "MODIFICADO", 4321.12));
+		
+		System.out.println(bilbao);
+	}
+}
