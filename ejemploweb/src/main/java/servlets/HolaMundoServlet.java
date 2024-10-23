9e307e85238032f683804b583219ea063785c9a0
Javier Lete <javier@eskura21.com>
2024-10-23 10:58:27 +0200

    HTML dentro de servlet

--- ejemploweb/src/main/java/servlets/HolaMundoServlet.java
+++ ejemploweb/src/main/java/servlets/HolaMundoServlet.java
@@ -14,13 +14,22 @@ public class HolaMundoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/plain");
+		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
 		
 		out.println("""
-				Hola
-				Este es un texto enviado desde una Servlet escrita en Java
+				<!DOCTYPE html>
+				<html>
+				<head>
+				<title>Ejemplo de Servlet</title>
+				</head>
+				<body>
+					<h1>Ejemplo de Servlet</h1>
+					<p>Hola</p>
+					<p>Este es un texto enviado desde una Servlet escrita en Java</p>
+				</body>
+				</html>
 				""");
 	}
 }
