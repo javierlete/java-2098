36a5ec240069d52eee443baf17efb3be9b196538
Javier Lete <javier@eskura21.com>
2024-10-30 09:57:47 +0100

    Pasamos el listado de videos de la base de datos a la pantalla

--- IparTube/src/main/java/ipartube/controladores/IndexServlet.java
+++ IparTube/src/main/java/ipartube/controladores/IndexServlet.java
@@ -7,11 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import ipartube.accesodatos.VideoDao;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("videos", VideoDao.obtenerTodos());
 		request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request, response);
 	}
 }
