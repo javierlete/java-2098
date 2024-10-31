da65dfd9d93e41877bf6c72ae76cf4c000221f3d
Javier Lete <javier@eskura21.com>
2024-10-31 10:32:01 +0100

    Exigir integridad referencial (foreign keys)

--- IparTube/src/main/java/ipartube/accesodatos/Globales.java
+++ IparTube/src/main/java/ipartube/accesodatos/Globales.java
@@ -1,5 +1,5 @@
 package ipartube.accesodatos;
 
 public class Globales {
-	static final String url = "jdbc:sqlite:C:\\Users\\html.IPARTEKAULA\\git\\java-2098\\IparTube\\bdd\\ipartube.db";
+	static final String url = "jdbc:sqlite:C:\\Users\\html.IPARTEKAULA\\git\\java-2098\\IparTube\\bdd\\ipartube.db?foreign_keys=true";
 }
