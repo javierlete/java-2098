251405a18fb87fd2495e5a4a779549b299674771
Javier Lete <javier@eskura21.com>
2024-11-04 09:28:58 +0100

    Modelo de autor con email y password

--- IparTube/src/main/java/ipartube/modelos/Autor.java
+++ IparTube/src/main/java/ipartube/modelos/Autor.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 
 public class Autor {
 	private Long id;
+	private String email;
+	private String password;
 	private String nombre;
 	private String descripcion;
 
 	private ArrayList<Video> videos = new ArrayList<>();
 
-	public Autor(Long id, String nombre, String descripcion) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.descripcion = descripcion;
+	public Autor(Long id, String email, String password, String nombre, String descripcion) {
+		setId(id);
+		setEmail(email);
+		setPassword(password);
+		setNombre(nombre);
+		setDescripcion(descripcion);
 	}
 
 	public Long getId() {
@@ -22,6 +25,22 @@ public class Autor {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getNombre() {
