88cf05b2aa60221632dfd33bd45cf1aa14a181af
Javier Lete <javier@eskura21.com>
2024-11-06 11:12:40 +0100

    Simulacro en index.jsp -> simulacro en IndexServlet. index.jsp terminada

--- ipartex/src/main/java/ipartex/controladores/IndexServlet.java
+++ ipartex/src/main/java/ipartex/controladores/IndexServlet.java
@@ -1,17 +1,39 @@
 package ipartex.controladores;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import ipartex.modelos.Mensaje;
+import ipartex.modelos.Usuario;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var mensajes = new ArrayList<Mensaje>();
+		
+		Usuario javier = new Usuario(1L, "Javier Lete", "javier@email.net", null);
+		Usuario pepe = new Usuario(2L, "Pepe Pérez", "pepe@email.net", null);
+		
+		Mensaje mensaje1 = new Mensaje(1L, javier, LocalDateTime.now(), "En una clase de informática de cuyo nombre no me da la gana de acordarme");
+		
+		mensaje1.getUsuariosLesGusta().add(pepe);
+		mensaje1.getUsuariosLesGusta().add(javier);
+		
+		mensajes.add(mensaje1);
+		mensajes.add(new Mensaje(2L, pepe, LocalDateTime.now(), "me da la gana de acordarme"));
+		mensajes.add(new Mensaje(3L, pepe, LocalDateTime.now(), "En una clase de informática me da la gana de acordarme"));
+		mensajes.add(new Mensaje(4L, javier, LocalDateTime.now(), "nombre no me da la gana de acordarme"));
+		
+		request.setAttribute("mensajes", mensajes);
+		
 		request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request, response);
 	}
 
--- ipartex/src/main/webapp/WEB-INF/vistas/index.jsp
+++ ipartex/src/main/webapp/WEB-INF/vistas/index.jsp
@@ -1,29 +1,31 @@
+<%@page import="ipartex.modelos.Mensaje"%>
+<%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ include file="/WEB-INF/vistas/includes/cabecera.jsp"%>
 
 <%
-for (int i = 1; i <= 20; i++) {
+@SuppressWarnings("unchecked")
+ArrayList<Mensaje> mensajes = (ArrayList<Mensaje>)request.getAttribute("mensajes");
+
+for (Mensaje m: mensajes) {
 %>
 
 <div class="card mb-3">
 	<div class="row g-0">
 		<div class="col-auto">
-			<img src="https://picsum.photos/40/40?<%=i%>"
+			<img src="https://picsum.photos/40/40?<%=m.getUsuario().getId()%>"
 				class="img-fluid rounded-circle mt-3 ms-3" alt="...">
 		</div>
 		<div class="col">
 			<div class="card-body">
 				<h5 class="card-title">
-					Usuario
-					<%=i%>
-					<small class="fs-6 text-body-secondary"><%=java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))%></small>
+					<%=m.getUsuario().getNombre()%>
+					<small class="fs-6 text-body-secondary"><%=m.getFechaFormateada()%></small>
 				</h5>
-				<p class="card-text">This is a wider card with supporting text
-					below as a natural lead-in to additional content. This content is a
-					little bit longer.</p>
+				<p class="card-text"><%=m.getTexto() %></p>
 				<p class="card-text">
-					<small class="text-body-secondary"><i class="bi bi-heart"></i> <%=i*100 %></small>
+					<small class="text-body-secondary"><i class="bi bi-heart"></i> <%=m.getNumeroLesGusta() %></small>
 				</p>
 			</div>
 		</div>
