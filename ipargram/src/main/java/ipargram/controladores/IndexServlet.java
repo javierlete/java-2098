401ee1b9f8d99d764c17a56596c490e5c49e930f
Javier Lete <javier@eskura21.com>
2024-11-13 10:27:21 +0100

    Controlador

---
+++ ipargram/src/main/java/ipargram/controladores/IndexServlet.java
@@ -0,0 +1,23 @@
+package ipargram.controladores;
+
+import java.io.IOException;
+
+import ipargram.accesodatos.EntradaAccesoDatos;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var entradas = EntradaAccesoDatos.obtenerTodas();
+		
+		request.setAttribute("entradas", entradas);
+		
+		request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request, response);
+	}
+}
