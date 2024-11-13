590ca832b55e0d1eb146b2da16a641c19b7706ef
Javier Lete <javier@eskura21.com>
2024-11-13 10:44:47 +0100

    Entrada: AccesoDatos

--- ipargram/src/main/java/ipargram/accesodatos/EntradaAccesoDatos.java
+++ ipargram/src/main/java/ipargram/accesodatos/EntradaAccesoDatos.java
@@ -14,9 +14,10 @@ public class EntradaAccesoDatos {
 	private static final String URL = "jdbc:sqlite:C:\\Users\\html.IPARTEKAULA\\git\\java-2098\\ipargram\\bdd\\ipargram.db";
 	private static final String USER = "";
 	private static final String PASS = "";
-	
+
 	private static final String SQL_SELECT = "SELECT * FROM entradas";
-	
+	private static final String SQL_SELECT_ID = SQL_SELECT + " WHERE id=?";
+
 	static {
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -24,25 +25,46 @@ public class EntradaAccesoDatos {
 			throw new RuntimeException("No se ha encontrado el driver", e);
 		}
 	}
-	
+
 	public static ArrayList<Entrada> obtenerTodas() {
 		var entradas = new ArrayList<Entrada>();
-		
+
 		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
 				PreparedStatement pst = con.prepareStatement(SQL_SELECT);
 				ResultSet rs = pst.executeQuery()) {
 			Entrada entrada;
-			
-			while(rs.next()) {
-				entrada = new Entrada(rs.getLong("id"), rs.getString("titulo"), LocalDateTime.parse(rs.getString("fecha_hora")));
-				
+
+			while (rs.next()) {
+				entrada = new Entrada(rs.getLong("id"), rs.getString("titulo"),
+						LocalDateTime.parse(rs.getString("fecha_hora")));
+
 				entradas.add(entrada);
 			}
-			
+
 			return entradas;
 		} catch (SQLException e) {
 			throw new RuntimeException("Error en la consulta", e);
 		}
 	}
-	
+
+	public static Entrada obtenerPorId(Long id) {
+		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
+				PreparedStatement pst = con.prepareStatement(SQL_SELECT_ID);) {
+
+			pst.setLong(1, id);
+
+			ResultSet rs = pst.executeQuery();
+
+			Entrada entrada = null;
+
+			if (rs.next()) {
+				entrada = new Entrada(rs.getLong("id"), rs.getString("titulo"),
+						LocalDateTime.parse(rs.getString("fecha_hora")));
+			}
+
+			return entrada;
+		} catch (SQLException e) {
+			throw new RuntimeException("Error en la consulta", e);
+		}
+	}
 }
